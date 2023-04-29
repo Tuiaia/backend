3bb896794c7a13f5bedc775ddb26941a854d7030
Rodrigo Stuqui Monzani <rst.monzani@gmail.com>
2023-04-29 08:04:12 -0400

    fix(Noticias): envia os conteudos de forma individual

--- Scraping/BoraInvestir.py
+++ Scraping/BoraInvestir.py
@@ -7,12 +7,7 @@ import json
 class BoraInvestir:
 
     def get_urls(self):
-        list_url = []
-        list_time = []
-        list_title = []
-        list_author = []
-        list_subtitle = []
-        list_font = []
+        r = redis.Redis(host='localhost', port=6379, db=0)
         yesterday = date.today() - timedelta(days=1)
         yesterday = date.strftime(yesterday, "%Y-%m-%d")
         for i in range(1, 5):
@@ -27,20 +22,13 @@ class BoraInvestir:
             time = [x.get('datetime') for x in time]
             time = [x for x in time if x[:10] == yesterday]
             url = url[:len(time)]
-            for x in url:
-                content = self.get_content(x)
-                list_title.append(content['title'])
-                list_author.append(content['author'])
-                list_subtitle.append(content['subtitle'])
-                list_font.append("BoraInvestir")
-            list_url = list_url + url
-            list_time = list_time + time
-
-        return {'data' : list_time, 'titulo' : list_title, 'url' : list_url, 'autor' : list_author, 'subtitulo' : list_subtitle, 'fonte' : list_font}
+            for x, data in zip(url, time):
+                content = self.get_content(x, data)
+                r.publish('canal_scraping', json.dumps(content))
 
         
 
-    def get_content(self, url):
+    def get_content(self, url, data):
         print(f"Acessando o link")
         response = requests.get(url)
         response.raise_for_status()
@@ -54,9 +42,7 @@ class BoraInvestir:
             subtitle = soup.find('h2', class_="page-content__subtitle").text
         except:
             subtitle = ""
-        return {'title' : title, 'author' : author, 'subtitle' : subtitle}
+        return {'title' : title, 'author' : author, 'subtitle' : subtitle, 'url' : url, 'data' : data}
     
     def run(self):
-        data = self.get_urls()
-        r = redis.Redis(host='localhost', port=6379, db=0)
-        r.publish('canal_scraping', json.dumps(data))
+        self.get_urls()
--- Scraping/Forbes.py
+++ Scraping/Forbes.py
@@ -6,11 +6,7 @@ import json
 
 class Forbes:
     def get_urls(self):
-        list_time = []
-        list_title = []
-        list_author = []
-        list_subtitle = []
-        list_font = []
+        r = redis.Redis(host='localhost', port=6379, db=0)
         yesterday = date.today() - timedelta(days=1)
         yesterday = date.strftime(yesterday, "%Y/%m/%d")
         response = requests.get(f'https://forbes.com.br/{yesterday}')
@@ -20,16 +16,10 @@ class Forbes:
         url = title.find_all('a', class_='link-title-post-1')
         url = [x.get('href') for x in url]
         for x in url:
-            content = self.get_content(x)
-            list_title.append(content['title'])
-            list_author.append(content['author'])
-            list_subtitle.append(content['subtitle'])
-            list_font.append("Forbes")
-            list_time.append(yesterday)
+            content = self.get_content(x, yesterday)
+            r.publish('canal_scraping', json.dumps(content))
 
-        return {'data' : list_time, 'titulo' : list_title, 'url' : url, 'autor' : list_author, 'subtitulo' : list_subtitle, 'fonte' : list_font}
-
-    def get_content(self, url):
+    def get_content(self, url, data):
         print(f"Acessando o link")
         response = requests.get(url)
         response.raise_for_status()
@@ -44,9 +34,7 @@ class Forbes:
             subtitle = soup.find('h2', class_="post__excerpt").text
         except:
             subtitle = ""
-        return {'title' : title, 'author' : author, 'subtitle' : subtitle}
+        return {'title' : title, 'author' : author, 'subtitle' : subtitle, 'url' : url, 'data' : data}
     
     def run(self):
-        data = self.get_urls()
-        r = redis.Redis(host='localhost', port=6379, db=0)
-        r.publish('canal_scraping', json.dumps(data))
+        self.get_urls()
