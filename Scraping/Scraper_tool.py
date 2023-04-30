1a92ea166e17952ef9762243a1465ef0473f6e18
Rodrigo Stuqui Monzani <rst.monzani@gmail.com>
2023-04-30 09:25:17 -0400

    fix(Raspagem): torna o parametro chamável nas funções

--- Scraping/Scraper_tool.py
+++ Scraping/Scraper_tool.py
@@ -4,8 +4,8 @@ import time
 import schedule
 
 if '__main__' == __name__:
-    schedule.every(2).hours.do(Forbes().run())
-    schedule.every(2).hours.do(BoraInvestir().run())
+    schedule.every(2).hours.do(Forbes().run)
+    schedule.every(2).hours.do(BoraInvestir().run)
     while True:
         schedule.run_pending()
         time.sleep(1)
